db90bdd52d0b22b1aa15021351e02d6f1ef35fa4
Priyanshi220404 <149704956+Priyanshi220404@users.noreply.github.com>
2025-09-03 12:30:38 +0530

    Add files via upload

---
+++ Data Processing/data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# -------------------------
+# Q1. Load the data in DataFrame (Pandas)
+# -------------------------
+df = pd.read_csv("melb_data.csv")
+# print("Q1: First 5 rows of dataset:\n", df.head())
+print(df.columns)
