a072d97424d11dbb24c3048b062ed04aa90fd8e0
Priyanshi220404 <149704956+Priyanshi220404@users.noreply.github.com>
2025-09-03 12:31:24 +0530

    Delete Data Processing directory

--- Data Processing/data.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# -------------------------
-# Q1. Load the data in DataFrame (Pandas)
-# -------------------------
-df = pd.read_csv("melb_data.csv")
-# print("Q1: First 5 rows of dataset:\n", df.head())
-print(df.columns)
